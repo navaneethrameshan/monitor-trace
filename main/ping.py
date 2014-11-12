f387b9a697d98c50a1ced182dbdf4961704b92eb
navaneethrameshan <navaneeth.rameshan@gmail.com>
2014-11-12 01:25:21 +0100

    Ping summary and transmit 5 packets

--- main/ping.py
+++ main/ping.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 def ping6(ip):
-    ping = subprocess.Popen(["ping6","-c","2", ip],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ping = subprocess.Popen(["ping6","-q","-c","5", ip],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     ping.wait()
     if ping.returncode != 0:
         #print ping.returncode, "ERROR: failed to ping host. Please check."
